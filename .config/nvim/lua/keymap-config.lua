ddc1508c212d07b7f65d46137922b6d52daf87b7
Kenta Konagaya <k.konagaya@kufusha.com>
2024-10-07 15:42:33 +0900

    add hop.nvim

---
+++ .config/nvim/lua/keymap-config.lua
@@ -0,0 +1,4 @@
+vim.keymap.set('n', '<leader>j', ':HopLine<CR>')
+vim.keymap.set('n', '<leader>k', ':HopWord<CR>')
+vim.keymap.set('v', '<leader>j', '<Cmd>HopLine<CR>')
+vim.keymap.set('v', '<leader>k', '<Cmd>HopWord<CR>')
